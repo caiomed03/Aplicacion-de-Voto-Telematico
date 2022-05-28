8fc1d4cf01c1896c5b2c2ead86db0e3d6d490569
Caio Medeiros Carvalho <caiolk960@gmail.com>
2022-05-28 11:51:40 +0200

    Agregar archivos de proyecto.

---
+++ Interfaz.cs
@@ -0,0 +1,10 @@
+namespace Aplicación_de_Voto_Telemático
+{
+    public partial class Interfaz : Form
+    {
+        public Interfaz()
+        {
+            InitializeComponent();
+        }
+    }
+}
